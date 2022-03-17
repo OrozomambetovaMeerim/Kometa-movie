297d6d0bf2362c86368b178c9ebd822c8a50b527
Yrysgul <yrysgulergesh@gmail.com>
2022-03-17 14:26:51 +0600

    added registration and authorization

--- main/views.py
+++ main/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from main.models import *
 from .forms import MovieForm, MessageForm, SearchForm
 from django.views.generic import TemplateView, ListView, View
 from django.db.models import Q
 from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -173,4 +181,71 @@ class DialogsViews(View):
     def get(self, request):
         message = Message.objects.get(id=id)
         return render(request, 'main/message.html', {'get': message})
+
+
+# def signupuser(request):
+#     if request.method == 'GET':
+#         return render(request, 'auth1/signupuser.html', {'form':UserCreationForm()})
+#     else:
+#         if request.POST['password1'] == request.POST['password2']:
+#             try:
+#                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+#                 user.save()
+#                 login(request, user)
+#                 return redirect('main/index.html')
+#             except IntegrityError:
+#                 return render(request, 'auth1/signupuser.html', {'form':UserCreationForm(), 'error':'That username has already been taken. Please choose a new username'})
+#         else:
+#             return render(request, 'auth1/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+# def loginuser(request):
+#     if request.method == 'GET':
+#         return render(request, 'auth1/loginuser.html', {'form':AuthenticationForm()})
+#     else:
+#         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+#         if user is None:
+#             return render(request, 'auth1/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+#         else:
+#             login(request, user)
+#             return redirect('main/index.html')
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render (request, 'auth1/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return render(request, 'main/index.html')
+            except IntegrityError:
+                return render(request, 'auth1/signupuser.html', {'forum':UserCreationForm(), 'error':'Это имя пользователя уже занято. Пожалуйста, выберите новое имя пользователя'})
+        else:
+            return render(request, 'auth1/signupuser.html', {'forum':UserCreationForm(), 'error':'Пароль не подходит'})
+    
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('main/index.html')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render (request, 'auth1/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'auth1/loginuser.html', {'form':AuthenticationForm(), 'error':'логин и пароль не совпадают'})
+        else:
+            login(request, user)
+            return render(request, 'main/index.html') 
+           
+
+# @login_required
+# def logoutuser(request):
+#     if request.method == 'POST':
+#         logout(request)
+#         return redirect('home')
+
+
 
